0aa7b367731108c29035adfa712ce51057f1d2f5
Jesse McReady <jessemcready@gmail.com>
2018-06-20 02:00:14 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,7 +12,12 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
-  
+  rooms = 1..attendees.size
+  assigned = []
+  for room in rooms
+    assigned << "Hello, #{attendees[room]}! You'll be assigned to room #{room}!"
+  end
+  assigned
 end
 
 def printer(attendees)
