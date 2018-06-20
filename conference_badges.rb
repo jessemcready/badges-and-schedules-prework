9877ee401bfe3d6a98347f3cfceb7ef800e5a618
Jesse McReady <jessemcready@gmail.com>
2018-06-20 01:56:00 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,3 +2,15 @@
 def badge_maker(name)
   "Hello, my name is #{name}."  
 end
+
+def batch_badge_creator(attendees)
+    
+end
+
+def assign_rooms(attendees)
+  
+end
+
+def printer(attendees)
+  
+end
